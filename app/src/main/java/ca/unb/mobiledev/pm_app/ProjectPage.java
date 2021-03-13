f7c2a3723d225e17e59cd695ecd72648e52e73ec
suemelon <semcinnis94@gmail.com>
2021-03-13 14:18:37 -0400

    commited minor changes
    minor changes

--- app/src/main/java/ca/unb/mobiledev/pm_app/ProjectPage.java
+++ app/src/main/java/ca/unb/mobiledev/pm_app/ProjectPage.java
@@ -2,25 +2,41 @@ package ca.unb.mobiledev.pm_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ca.unb.mobiledev.pm_app.Model.Projects;
+
 public class ProjectPage extends AppCompatActivity {
 
-    private TextView projectNameTextView;
+    private TextView teamPage;
+    private TextView tasks;
+    private TextView chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projectpage);
 
-        projectNameTextView = findViewById(R.id.tv_projectname);
 
-        Intent intent = getIntent();
-        String projectId = intent.getStringExtra("projectId");
-        String projectName = intent.getStringExtra("projectName");
-        projectNameTextView.setText(projectName);
+        //click listener to view tasks list for specific project
+        tasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tasksIntent = new Intent(ProjectPage.this, TasksList.class);
+                startActivity(tasksIntent);
+            }
+        });
 
     }
 }
+
+//
+//       projectNameTextView = findViewById(R.id.tv_projectname);
+//
+//               Intent intent = getIntent();
+//               String projectId = intent.getStringExtra("projectId");
+//               String projectName = intent.getStringExtra("projectName");
+//               projectNameTextView.setText(projectName);
