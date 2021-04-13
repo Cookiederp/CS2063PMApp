4b1c050f550298aaf12aeb9b5f05757cc49a090d
Cookiederp <73700220+Cookiederp@users.noreply.github.com>
2021-04-12 22:33:42 -0300

    Removed debug toast and added request permission for reading storage.

--- app/src/main/java/ca/unb/mobiledev/pm_app/UserSettings.java
+++ app/src/main/java/ca/unb/mobiledev/pm_app/UserSettings.java
@@ -1,9 +1,11 @@
 package ca.unb.mobiledev.pm_app;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -38,6 +40,7 @@ import java.util.HashMap;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.exifinterface.media.ExifInterface;
 
 
@@ -87,7 +90,9 @@ public class UserSettings extends AppCompatActivity {
         changeIconButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SelectImage();
+                ActivityCompat.requestPermissions(UserSettings.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
             }
         });
 
@@ -103,6 +108,21 @@ public class UserSettings extends AppCompatActivity {
         i.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(i, IMAGE_REQUEST);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SelectImage();
+                } else {
+                    Toast.makeText(UserSettings.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
     private String getFileExtension(Uri uri){
